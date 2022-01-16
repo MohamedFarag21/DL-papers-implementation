ca244b09e34372b965bcbbfc919f0bfed8d20f77
Mohamed Mohamed Farag <31109954+MohamedFarag21@users.noreply.github.com>
2022-01-16 04:37:09 -0800

    Update SAM.py

--- Computer_vision/CBAM: Convolutional Block Attention Module/Code/SAM.py
+++ Computer_vision/CBAM: Convolutional Block Attention Module/Code/SAM.py
@@ -1,18 +1,18 @@
 class sp_att(tf.keras.layers.Layer):
 
   def __init__(self):
-    super(sp_att,self).__init__(name='Spatial_attention')
+      super(sp_att,self).__init__(name='Spatial_attention')
 
-    # self.kernel_size = kernel_size
+      # self.kernel_size = kernel_size
 
-    self.concat = tf.keras.layers.Concatenate(axis=-1)
-    self.conv   = tf.keras.layers.Conv2D(filters = 1, 
-                                         kernel_size=7, 
-                                         padding='same',  
-                                         strides = 1, 
-                                         activation = 'sigmoid',  
-                                         kernel_initializer='he_normal',
-                                         use_bias = False)
+      self.concat = tf.keras.layers.Concatenate(axis=-1)
+      self.conv   = tf.keras.layers.Conv2D(filters = 1, 
+                                           kernel_size=7, 
+                                           padding='same',  
+                                           strides = 1, 
+                                           activation = 'sigmoid',  
+                                           kernel_initializer='he_normal',
+                                           use_bias = False)
 
   def call(self,inputs):
       
@@ -22,12 +22,5 @@ class sp_att(tf.keras.layers.Layer):
       x   = self.concat([x11, x22])
       x   = self.conv(x)
       x   = tf.keras.layers.multiply([x, inputs])
-
-    
-
-    # def get_config(self):
-    #   return {'kernel_size': self.kernel_size},
              
-       
-
-    return x
+      return x
